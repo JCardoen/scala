12845277680b8b3cf4be931911eaf5e88918193b
JCardoen <cardoen.joachim@gmail.com>
2017-06-09 15:56:18 +0200

    Week 1 Assignment done

--- src/main/scala/recfun/src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/src/main/scala/recfun/Main.scala
@@ -8,6 +8,13 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println("Balancing paranthesis")
+    println(balance("(just) an example".toList))
+    println(balance("(just an example".toList))
+    println(balance("())(".toList))
+    var list = List[Int](1,2)
+    println(countChange(5,list ))
   }
 
   /**
@@ -15,17 +22,38 @@ object Main {
    */
     def pascal(c: Int, r: Int): Int =
       if(c == 0 || r == c) 1
-      else pascal(c + pascal(c, r-1), r)
+      else pascal(c, r-1) + pascal(c-1, r-1)
 
 
-  
+  var test = List[Char]()
+
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      def checkOpen(chars: List[Char], openParenthesis: Int): Boolean = {
+        if(chars.isEmpty) openParenthesis == 0
+        else {
+          val parenthesisCount =
+            if(chars.head.equals('(')) openParenthesis + 1
+            else if(chars.head.equals(')')) openParenthesis - 1
+            else openParenthesis
+
+          if(parenthesisCount >= 0) checkOpen(chars.tail, parenthesisCount)
+          else false
+        }
+      }
+      checkOpen(chars, 0)
+    }
+
+
+
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int =
+      if(coins.isEmpty || money < 0) 0
+      else if(money == 0) 1
+      else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
